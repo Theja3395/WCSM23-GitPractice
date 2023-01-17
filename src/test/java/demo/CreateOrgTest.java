6680cac7f0ffc407b60c2a10f0caf7e86160175b
LENOVO <LENOVO@DESKTOP-L0CE2LE>
2023-01-17 18:53:58 +0530

    commit and push again

--- src/test/java/demo/CreateOrgTest.java
+++ src/test/java/demo/CreateOrgTest.java
@@ -4,6 +4,10 @@ import org.testng.annotations.Test;
 public class CreateOrgTest {
 	@Test
 public void Test1() {
-	System.out.println("create org");
+	System.out.println("Demo test-1");
 }
+	@Test
+	public void Test2() {
+		System.out.println("Demo test-2");
+	}	
 }
